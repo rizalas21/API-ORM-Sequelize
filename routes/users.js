ac7bdb6ef60122007d92e47b96b2bc657c45e656
rizal anggara sudrajat <sudrajatrizal1@gmail.com>
2023-11-08 11:10:33 +0700

    Api Sequelize

--- routes/users.js
+++ routes/users.js
@@ -36,6 +36,7 @@ router.get('/phonebooks', async function (req, res, next) {
 router.post('/phonebooks', async function (req, res, next) {
   try {
     const { name, phone } = req.body
+    if(!name && !phone) res.status(500).json(new Error("name and phone don't be empty"))
     const users = await models.User.create({ name, phone });
     res.status(201).json(users)
   } catch (err) {
@@ -47,25 +48,81 @@ router.post('/phonebooks', async function (req, res, next) {
 router.delete('/phonebooks/:id', async function (req, res, next) {
   try {
     const id = req.params.id
-    const deletedUser = await models.User.findOne({
+
+    const user = await models.User.findOne({
       where: {
         id: id
       }
     });
-
-    if (!deletedUser) {
-      return res.status(404).json({ message: 'Data not found' })
+    if (!user) {
+      res.status(400).json({ message: "user not found" })
     }
-
     await models.User.destroy({
       where: {
         id: id
       }
+    })
+    return res.status(200).json(user)
+
+  } catch (err) {
+    console.log('ini erorr dari get users =>', err)
+    res.status(500).json({ err })
+  }
+});
+
+router.put('/phonebooks/:id', async function (req, res, next) {
+  try {
+    const id = req.params.id
+    const { name, phone } = req.body
+
+    // updated di eksekusi saat menjalankan operasi update 
+    const [updated] = await models.User.update({ name, phone }, {
+      where: {
+        id: id
+      }
     });
 
-    res.status(200).json({
-      Response: deletedUser
-    })
+    if (updated === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const updatedUser = await models.User.findOne({
+      where: {
+        id: id
+      }
+    });
+
+    return res.status(201).json(updatedUser);
+
+  } catch (err) {
+    console.log('ini erorr dari get users =>', err)
+    res.status(500).json({ err })
+  }
+});
+
+router.put('/phonebooks/:id/avatar', async function (req, res, next) {
+  try {
+    const id = req.params.id
+    const { avatar } = req.body
+
+    // updated di eksekusi saat menjalankan operasi update 
+    const [updated] = await models.User.update({ avatar }, {
+      where: {
+        id: id
+      }
+    });
+
+    if (updated === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const updatedUser = await models.User.findOne({
+      where: {
+        id: id
+      }
+    });
+
+    return res.status(200).json(updatedUser);
 
   } catch (err) {
     console.log('ini erorr dari get users =>', err)
